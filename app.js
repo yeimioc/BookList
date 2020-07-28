643b228aef0e2cb6aa9795c7cdbd323f5261466f
Yeimi <carmen-2019@hotmail.com>
2020-07-28 16:58:28 -0400

    Added functions to the page using JavaScript

--- app.js
+++ app.js
@@ -1,28 +1,47 @@
 //Book constructor
-/*function Book(title, author, isbn) {
+function Book(title, author, isbn) {
     this.title = title;
     this.author = author;
     this.isbn = isbn;
 }
 
 //UI Constructor
-function UI() {}*/
+function UI() {}
+
+//Add book to list
+UI.prototype.addBookToList = function(book){
+   const list = document.getElementById('book-list');
+
+   //Create tr element
+   const row = document.createElement('tr');
+
+   //Insert cols
+   row.innerHTML = `
+     <td>${book.title}</dt>
+     <td>${book.author}</dt>
+     <td>${book.isbn}</dt>
+     <td><a href="#" class="delete">X<a></td>
+   `;
+
+   list.appendChild(row);
+}
 
 //Event Listeners
 document.getElementById('book-form').addEventListener('submit', 
 function(e){
     //Get form values
     const title = document.getElementById('title').value,
-    author = document.getElementById('author').value,
-    isbn = document.getElementById('isbn').value
-
-   
+          author = document.getElementById('author').value,
+          isbn = document.getElementById('isbn').value
     
    //Instantiate book
    const book = new Book(title, author,isbn);
 
-   console.log(book);
-    
+   //Instantiate UI
+   const ui = new UI();
+
+   //Add book to list
+   ui.addBookToList(book);
 
     e.preventDefault();
 });
